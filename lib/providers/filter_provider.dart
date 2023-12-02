4e6333ecf6d7e3a464f8004c69a6fd2b82151afb
EvanSimon <41878176+EvanSimon@users.noreply.github.com>
2023-12-02 12:35:44 -0500

    Filters file updated to use the provider

--- lib/providers/filter_provider.dart
+++ lib/providers/filter_provider.dart
@@ -9,12 +9,26 @@ enum Filter {
 }
 
 class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
-  FiltersNotifier() : super({
-    Filter.glutenFree: false,
-    Filter.lactoseFree: false,
-    Filter.vegetarian: false,
-    Filter.vegan: false,
-  });
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFilters(Map<Filter, bool> chosenFilters) {
+    state = chosenFilters;
+  }
+
+  void setFilter(Filter filter, bool isActive) {
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
 }
 
-final filtersProvider = StateNotifierProvider((ref) => null);
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+        (ref) => FiltersNotifier());
